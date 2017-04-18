2dbcf707d6aedf737775333bee3531f165a07c99
Lucas Pickering <lucaspickering9@gmail.com>
2017-04-18 19:50:46 -0400

    Added some more collectors in Pair

--- src/main/java/me/lucaspickering/utils/Pair.java
+++ src/main/java/me/lucaspickering/utils/Pair.java
@@ -1,10 +1,16 @@
 package me.lucaspickering.utils;
 
 import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Pair<T, U> {
+
+    private static <T> BinaryOperator<T> throwingMerger() {
+        return (u,v) -> { throw new IllegalStateException(String.format("Duplicate key %s", u)); };
+    }
 
     private final U second;
     private final T first;
@@ -18,12 +24,23 @@ public class Pair<T, U> {
      * Returns a {@link Collector} that will convert a {@link java.util.stream.Stream} of pairs
      * into a {@link Map} of those same pairs.
      *
-     * @param <T> the type of the first value in each pair, to become the type of each key
-     * @param <U> the type of the second value in each pair, to become the type of each value
+     * @param <K> the type of the first value in each pair, to become the type of each key
+     * @param <V> the type of the second value in each pair, to become the type of each value
      * @return a {@link Collector} that will turn pairs into a {@link Map}
      */
-    public static <T, U> Collector<Pair<T, U>, ?, Map<T, U>> mapCollector() {
+    public static <K, V> Collector<Pair<K, V>, ?, Map<K, V>> mapCollector() {
         return Collectors.toMap(Pair::first, Pair::second);
+    }
+
+    public static <K, V, M extends Map<K, V>>
+    Collector<Pair<K, V>, ?, M> toMap(Supplier<M> mapSupplier) {
+        return Collectors.toMap(Pair::first, Pair::second, throwingMerger(), mapSupplier);
+    }
+
+    public static <K, V, M extends Map<K, V>>
+    Collector<Pair<K, V>, ?, M> toMap(BinaryOperator<V> mergeFunction,
+                                      Supplier<M> mapSupplier) {
+        return Collectors.toMap(Pair::first, Pair::second, mergeFunction, mapSupplier);
     }
 
     public T first() {
